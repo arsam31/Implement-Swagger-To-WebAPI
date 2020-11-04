b2d0919e41cea213e63ef70c0775495db5ac6b3d
quaestor_aa <aa@softwarealliance.dk>
2020-11-04 12:27:22 +0500

    Add project files.

---
+++ ShopingMall.API/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopingMall.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[]
+                    { "Hello World!",
+                    "Hello Galaxy!",
+                    "Hello Universe!"
+                    };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Hello";
+        }
+
+        [HttpPost]
+        public string Post([FromBody] string value)
+        {
+            return "Hello World!";
+        }
+
+
+        [HttpPut("{id}")]
+        public string Put(int id, [FromBody] string value)
+        {
+            return "Hello Galaxy!";
+        }
+
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "Hello Universe!";
+        }
+    }
+}
